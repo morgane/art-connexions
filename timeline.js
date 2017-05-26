046f694d4240c7a37859544cce627e3cb4455ec7
Morgane Santos <morganesantos@gmail.com>
2017-05-26 00:02:48 -0700

    i cant believe listening to Blow the Whistle for an hour straight panned out for me

--- timeline.js
+++ timeline.js
@@ -1,70 +1,95 @@
-function timeline(domElement) {
-  var width = 1000,
-      height = 10000,
-      padding = 100;
 
-  var svg = d3.select("body")
-              .append("svg")
-              .attr("width", width)
-              .attr("height", height)
-              .append("g");
+var width = 1000,
+    height = 10000,
+    padding = 100;
 
-  var mindate = new Date(1400, 0, 1),
-      maxdate = new Date(2012, 0, 31);
+var dateArray = [];
 
-  var yScale = d3.time.scale()
-                      .range([padding, height - (padding * 2)])
-                      .domain([mindate, maxdate]);
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g");
 
-  var yAxis = d3.svg.axis()
-                    .orient("left")
-                    .scale(yScale);
+var mindate = new Date(1400, 0, 1),
+    maxdate = new Date(2012, 0, 31);
 
-  d3.json("art-connexions-export.json", function(data) {
-    var date;
-    var dateArray = [];
+var yScale = d3.time.scale()
+                    .range([padding, height - (padding * 2)])
+                    .domain([mindate, maxdate]);
 
-    data.nodes.forEach(function(d) {
-      date = new Date(d.date, 0);
-      d.date = date;
-      dateArray[d.id] = date;
-    });
+var yAxis = d3.svg.axis()
+                  .orient("left")
+                  .scale(yScale);
 
-    var line = svg.attr("class", "line")
-                  .selectAll("line").data(data.links)
-                  .enter().append("line")
-                  .style("stroke", "black");
+var force = d3.layout.force()
+                     .size([width, height])
+                     .charge(-300)
+                     .linkDistance(100)
+                     .on("tick", tick);
 
-    var node = svg.selectAll(".node")
-                  .data(data.nodes)
-                  .enter().append("g")
-                  .attr("class", "node");
+var drag = force.drag()
+                .on("dragstart", dragstart);
 
-    node.append("circle")
-        .attr("stroke", "black")
-        .attr("fill", "white")
-        .attr("r", 40)
-        .attr("cx", 200)
-        .attr("cy", function(element) { return yScale(element.date); });
+var link = svg.selectAll(".link"),
+    node = svg.selectAll(".node");
 
-    node.append("text")
-        .attr("dx", 200)
-        .attr("dy", function(element) { return yScale(element.date); })
-        .attr("text-anchor", "middle")
-        .text(function(element) {return element.id});
+d3.json("connexions.json", function(error, graph) {
+  if (error) throw error;
 
-    line.attr("x1", 200)
-        .attr("y1", function(element) { return findDate(element.source); })
-        .attr("x2", 200)
-        .attr("y2", function(element) { return findDate(element.target); });
-
-    function findDate(name) {
-      return yScale(dateArray[name]);
-    }
-
-    svg.append("g")
-       .attr("class", "yaxis")
-       .attr("transform", "translate("+padding+", 0)")
-       .call(yAxis);
+  graph.nodes.forEach(function(d) {
+    date = new Date(d.date, 0);
+    d.date = date;
   });
+
+  force
+      .nodes(graph.nodes)
+      .links(graph.links)
+      .start();
+
+  link = link.data(graph.links)
+             .enter().append("line")
+             .attr("class", "link")
+             .style("stroke", "black");
+
+  node = node.data(graph.nodes)
+             .enter().append("g")
+             .attr("class", "node")
+             .on("dblclick", dblclick)
+             .call(drag);
+
+  node.append("circle")
+    .attr("r", 40)
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("stroke", "black")
+    .attr("fill", "white");
+
+  node.append("text")
+      .attr("dx", 0)
+      .attr("dy", 0)
+      .text(function(d) { return d.name });
+
+});
+
+function tick() {
+  link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(element) { return yScale(element.source.date); })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(element) { return yScale(element.target.date); });
+
+  node.attr("transform", function(d) { return "translate(" + d.x + "," + yScale(d.date) + ")"; });
 }
+
+function dblclick(d) {
+  d3.select(this).classed("fixed", d.fixed = false);
+}
+
+function dragstart(d) {
+  d3.select(this).classed("fixed", d.fixed = true);
+}
+
+svg.append("g")
+ .attr("class", "yaxis")
+ .attr("transform", "translate("+padding+", 0)")
+ .call(yAxis);
