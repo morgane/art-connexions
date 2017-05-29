68dee380444abfdb7debed83d280ad1e1bfc90e6
Morgane Santos <morganesantos@gmail.com>
2017-05-29 08:30:48 -0700

    shoutout to isha

---
+++ styling.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $(".node image").each(function() {
+    // Calculate aspect ratio of SVG images
+    var svgHeight = $(this)[0].getBoundingClientRect().height;
+    var svgWidth = $(this)[0].getBoundingClientRect().width;
+    var aspectRatio = svgWidth/svgHeight;
+
+    if (aspectRatio > 1) { // Image is landscape
+      $(this).css({height: "100px"});
+      svgWidth = $(this)[0].getBoundingClientRect().width; // get new width
+      $(this).css({x: -svgWidth/2}); // center image
+    } else { // Image is portrait or square
+      $(this).css({width: "100px"});
+    }
+  });
+});
